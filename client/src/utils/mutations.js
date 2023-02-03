5216cf893bb1fa65e77ff6057bc63f5dd3ea2cf2
Andrew <andrewrandolph98@gmail.com>
2023-02-02 19:23:13 -0600

    Components structure added as well as mutations completed

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client'
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+
+export const ADD_POST = gql`
+mutation addPost($message: String!) {
+    addPost(message: $message) {
+      id
+      message
+      username
+      createdAt
+      tags
+      title
+      comments {
+        id
+        comment
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+mutation addPost($postId: ID!, $comment: String!) {
+    addComment(PostId: $postId, comment: $comment) {
+      id
+      message
+      title
+      username
+      createdAt
+      comments {
+        id
+        comment
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
