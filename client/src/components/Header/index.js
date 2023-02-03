840f24360e0aacf100fa60ebfc59b1f4f7366f09
Andrew <andrewrandolph98@gmail.com>
2023-02-02 21:51:10 -0600

    Added pages as well as fixed resolvers and typedefs.

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+
+const Header = () => {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout()
+    };
+    return (
+        <header className=''>
+            <div className=''>
+                <div>
+                    <Link>
+                        <h1>Derma:</h1>
+                    </Link>
+                    <p>Your Skin. Your Choice.</p>
+                </div>
+                <div>
+                    {Auth.loggedIn() ? (
+                        <>
+                            <Link>
+                                {Auth.getProfile().data.username}'s Profile
+                            </Link>
+                            <button className='' onClick={logout}></button>
+                        </>
+                    ) : (
+                        <>
+                            <Link className='' to="">
+                                login
+                            </Link>
+                            <Link className='' to="">
+                                Signup
+                            </Link>
+                        </>
+                    )
+                    }
+                </div>
+            </div>
+        </header>
+    )
+}
