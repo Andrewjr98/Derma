ed8cd9e73f2025452aa0fee7e62f6637ebb2d1f4
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-05 17:34:50 -0600

    adding CommentList and imports for profile

---
+++ client/src/pages/profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import ThoughtForm from '../components/PostForm';
+import ThoughtList from '../components/PostList';
+
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+import Auth from '../utils/auth';
+
+
